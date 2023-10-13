6987c2dec09b955727762b4b9e3ed06b33715b71
abraham <abraham.turnsek@gmail.com>
2023-10-13 14:01:03 +0200

    correccion menu y fuentes

---
+++ .next/static/9tLhoWcbWLvdZJW0Y4jpy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-0606bbedcc4ae4df.js","static/css/47054f3c599cc02c.css","static/chunks/pages/index-8043f1db61ec4e44.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
